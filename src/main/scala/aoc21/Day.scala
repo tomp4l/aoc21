dcb7a536d56eb355cf78631022eb5af508d9d9de
Tom Mottram <tom.mottram@kaluza.com>
2021-12-02 13:09:48 +0000

    Allow running all

--- src/main/scala/aoc21/Day.scala
+++ src/main/scala/aoc21/Day.scala
@@ -4,16 +4,6 @@ import cats.effect.IO
 import cats.syntax.all.*
 
 trait Day[A]:
-
-  def run(input: List[String]): IO[Unit] =
-    for
-      input <- parse(input)
-      part1 <- runPart1(input)
-      _ <- IO.println(s"Part 1 answer: $part1")
-      part2 <- runPart2(input)
-      _ <- IO.println(s"Part 2 answer: $part2")
-    yield ()
-
   def parse(input: List[String]): IO[List[A]]
   def runPart1(input: List[A]): IO[String]
   def runPart2(input: List[A]): IO[String]
@@ -30,8 +20,4 @@ trait StringDay extends Day[String]:
 
 trait IntDay extends Day[Int]:
   def parse(l: List[String]): IO[List[Int]] =
-    l.map(s =>
-      IO.fromOption(s.toIntOption)(
-        new Exception(s"Failed to parse int from $s")
-      )
-    ).sequence
+    l.map(_.toIntIO).sequence
--- src/main/scala/aoc21/Main.scala
+++ src/main/scala/aoc21/Main.scala
@@ -2,6 +2,7 @@ package aoc21
 
 import cats.effect.IOApp
 import cats.effect.IO
+import cats.syntax.all.*
 import fs2.io.file.Files
 import fs2.io.file.Path
 import fs2.{Stream, text}
@@ -17,15 +18,24 @@ object Main extends IOApp.Simple:
     for
       _ <- IO.println("Which day?")
       day <- IO.readLine
-        .map(_.toIntOption)
-        .flatMap(IO.fromOption(_)(new Exception("Couldn't parse day")))
-      program <- IO.fromOption(days.get(day))(
-        new Exception(s"Day $day is not defined yet")
-      )
-      _ <- IO.println(s"Running day $day")
-      input <- readInput(day)
-      part1 <- program.run(input)
+      _ <- day match
+        case "all" =>
+          days.keySet.map(runDay).toList.sequence
+        case i => i.toIntIO.flatMap(runDay)
     yield ()
+
+  private def runDay(day: Int) = for
+    program <- IO.fromOption(days.get(day))(
+      new Exception(s"Day $day is not defined yet")
+    )
+    _ <- IO.println(s"Running day $day")
+    input <- readInput(day)
+    parsed <- program.parse(input)
+    part1 <- program.runPart1(parsed)
+    _ <- IO.println(s"Part 1 answer: $part1")
+    part2 <- program.runPart2(parsed)
+    _ <- IO.println(s"Part 2 answer: $part2")
+  yield ()
 
   private def readInput(day: Int): IO[List[String]] =
     val filename = s"inputs/day$day.txt"
