15cdabb9ee319c7404db3b87753caab20f4e578d
Tom Mottram <tom.mottram@kaluza.com>
2021-12-11 10:36:34 +0000

    Use State for less readable code :)

--- src/main/scala/aoc21/day11/Program.scala
+++ src/main/scala/aoc21/day11/Program.scala
@@ -3,6 +3,7 @@ package day11
 
 import cats.syntax.all.*
 import cats.data.State
+import cats.Monad
 
 object Program extends PureDay:
   type A = Map[Point2d, Int]
@@ -18,20 +19,25 @@ object Program extends PureDay:
       .map(_.toMap)
 
   def simulateFlashes(map: A): (A, Int) =
-    val increased = map.map((k, v) => k -> (v + 1))
-    def loop(state: A, flashed: Set[Point2d]): (A, Int) =
-      val powered = state.filter((_, v) => v > 9).keySet
-      val toFlash = powered -- flashed
-      if toFlash.isEmpty then
-        (state.map((k, v) => k -> (if v > 9 then 0 else v)), flashed.size)
-      else
-        val toIncrease =
-          toFlash.toList.flatMap(p => p.neighbours ++ p.diagonals)
-        val nextState = toIncrease
-          .traverse(p => State.modify[A](_.updatedWith(p)(_.map(_ + 1))))
-          .runS(state)
-        loop(nextState.value, powered)
-    loop(increased, Set.empty)
+    Monad[[X] =>> State[A, X]]
+      .tailRecM(Set.empty[Point2d])(flashed =>
+        State
+          .inspect[A, Set[Point2d]](_.filter((_, v) => v > 9).keySet -- flashed)
+          .flatMap(toFlash =>
+            if toFlash.isEmpty then
+              State.inspect((state: A) =>
+                val setZero = state.map((k, v) => k -> (if v > 9 then 0 else v))
+                Right((setZero, flashed.size))
+              )
+            else
+              toFlash.toList
+                .flatMap(p => p.neighbours ++ p.diagonals)
+                .traverse(p => State.modify[A](_.updatedWith(p)(_.map(_ + 1))))
+                .as(Left(flashed ++ toFlash))
+          )
+      )
+      .runA(map.map((k, v) => k -> (v + 1)))
+      .value
 
   def part1(input: A): String =
     def run(i: Int, a: A, c: Int): String =
