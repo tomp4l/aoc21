a2efc2c0d7d3ccd41bbecf91cf00f6061d1cf794
Tom Mottram <tom.mottram@kaluza.com>
2021-12-20 13:00:17 +0000

    Keep track of min/max

--- src/main/scala/aoc21/day20/Program.scala
+++ src/main/scala/aoc21/day20/Program.scala
@@ -36,19 +36,21 @@ object Program extends PureDay:
 
   def padImage(
       image: Map[Point2d, Pixel],
-      padWith: Pixel
+      padWith: Pixel,
+      mins: Point2d,
+      maxs: Point2d
   ): Map[Point2d, Pixel] =
     val points = image.keySet
-    val minX = points.minBy(_.x).x
-    val minY = points.minBy(_.y).y
-    val maxX = points.maxBy(_.x).x
-    val maxY = points.maxBy(_.y).y
-    val xs = ((minX - 1) to (maxX + 1)).toSet
-    val ys = ((minY - 1) to (maxY + 1)).toSet
+    val minX = mins.x
+    val minY = mins.y
+    val maxX = maxs.x
+    val maxY = maxs.y
+    val xs = ((minX - 1) to (maxX + 1))
+    val ys = ((minY - 1) to (maxY + 1))
     val extraPoints =
-      xs.map(x => Point2d(x, minY - 1)) ++ xs.map(x => Point2d(x, maxY + 1)) ++
-        ys.map(y => Point2d(minX - 1, y)) ++ ys.map(y => Point2d(maxX + 1, y))
-    val toAdd = extraPoints.map(p => p -> padWith).toMap
+      xs.flatMap(x => Set(Point2d(x, minY - 1), Point2d(x, maxY + 1))) ++
+        ys.flatMap(y => Set(Point2d(minX - 1, y), Point2d(maxX + 1, y)))
+    val toAdd = extraPoints.map(p => p -> padWith)
     image ++ toAdd
 
   val pixelToInt =
@@ -71,29 +73,41 @@ object Program extends PureDay:
   val offsets = (-1 to 1).flatMap(y => (-1 to 1).map(x => Point2d(x, y))).toList
 
   def step(algorithm: Vector[Pixel]) =
-    State.modify[(Map[Point2d, Pixel], Pixel)]((image, infinityState) =>
-      val padded = padImage(image, infinityState)
-      padded.map((p, v) =>
-        val pixelValue =
-          offsets.map(relative => padded.getOrElse(p + relative, infinityState))
-        p -> algorithm(pixelToInt(pixelValue))
-      ) -> (infinityState match
-        case Pixel.Dark => algorithm.head
-        case Pixel.Light => algorithm.last
-      )
+    State.modify[(Map[Point2d, Pixel], (Pixel, (Point2d, Point2d)))](
+      (image, s) =>
+        val infinityState = s._1
+        val (mins, maxs) = s._2
+        val padded = padImage(image, infinityState, mins, maxs)
+        padded.map((p, v) =>
+          val pixelValue =
+            offsets.map(relative =>
+              padded.getOrElse(p + relative, infinityState)
+            )
+          p -> algorithm(pixelToInt(pixelValue))
+        ) -> ((infinityState match
+          case Pixel.Dark => algorithm.head
+          case Pixel.Light => algorithm.last
+        ) -> (mins + Point2d(-1, -1), maxs + Point2d(1, 1)))
     )
+
+  def startState(a: A) = (
+    a._2,
+    (Pixel.Dark, (Point2d(0, 0), a._2.keySet.maxBy(p => p.x + p.y)))
+  )
 
   def part1(input: A): String =
     val stepWithAlg = step(input._1)
     val twoSteps = stepWithAlg >> stepWithAlg
-    val ran = twoSteps.runS((input._2, Pixel.Dark)).value
+    val ran = twoSteps
+      .runS(startState(input))
+      .value
     ran._1.count(_._2 == Pixel.Light).toString
 
   def part2(input: A): String =
     val stepWithAlg = step(input._1)
     (1 to 50).toList
       .traverse(_ => stepWithAlg)
-      .runS((input._2, Pixel.Dark))
+      .runS(startState(input))
       .value
       ._1
       .count(_._2 == Pixel.Light)
